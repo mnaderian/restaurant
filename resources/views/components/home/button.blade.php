fcc9b34ab75f0b8b7c923d7a657e6e5284dd0ab8
Mahdi Naderian <naderian.mahdi@gmail.com>
2025-11-20 13:50:58 +0330

    removed target from home view links

--- resources/views/components/home/button.blade.php
+++ resources/views/components/home/button.blade.php
@@ -1,11 +1,8 @@
 @props([
     'link' => '#',
-    'target' => '_blank',
     'text' => '',
 ])
 
-<a  href="{{ $link }}" 
-    target="{{ $target }}" 
-    {{ $attributes->merge(['class' => 'inline-block hover:bg-amber-500 transition px-5 py-1.5 bg-sky-600 rounded-sm shadow text-white text-sm leading-normal']) }}>
+<a href="{{ $link }}" {{ $attributes->merge(['class' => 'inline-block hover:bg-amber-500 transition px-5 py-1.5 bg-sky-600 rounded-sm shadow text-white text-sm leading-normal']) }}>
     {{ $text }}
 </a>
