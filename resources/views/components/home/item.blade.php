06cef226f71b37ffeffa3898d32b237b88f49949
Mahdi Naderian <naderian.mahdi@gmail.com>
2025-11-20 12:07:14 +0330

    code cleanup

--- resources/views/components/home/item.blade.php
+++ resources/views/components/home/item.blade.php
@@ -1,4 +1,4 @@
-<li class="flex items-center gap-4 py-2 relative before:top-1/2 before:bottom-0 before:start-[0.4rem] before:absolute">
+<li class="flex items-center gap-3 py-1 relative before:top-1/2 before:bottom-0 before:start-[0.4rem] before:absolute">
     <span class="relative py-1 bg-white dark:bg-[#161615]">
         <span class="flex items-center justify-center rounded-full bg-[#FDFDFC] dark:bg-[#161615] shadow-[0px_0px_1px_0px_rgba(0,0,0,0.03),0px_1px_2px_0px_rgba(0,0,0,0.06)] w-3.5 h-3.5 d">
             <span class="rounded-full bg-[#dbdbd7] dark:bg-[#3E3E3A] w-1.5 h-1.5"></span>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>{{ config('app.name') }}</title>
-
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
@@ -38,22 +36,19 @@
                 </nav>
             @endif
         </header>
-        <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
+        <div class="flex items-center justify-center w-full lg:grow">
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
                 <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-es-lg rounded-ee-lg lg:rounded-ss-lg lg:rounded-ee-none">
-                    <h1 class="mb-3 font-black text-xl">سیستم مدیریت رستوران</h1>
-                    <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">سیستمی برای مدیریت رستوران و رزرو آنلاین</p>
+                    <h1 class="mb-1 font-black text-2xl">سیستم مدیریت رستوران</h1>
+                    <p class="mb-7 text-[#706f6c] dark:text-[#A1A09A]">سیستمی برای مدیریت رستوران و رزرو آنلاین</p>
                     <ul class="flex flex-col mb-4 lg:mb-6">
-                        <x-home.item>جهت ثبت نام از بخش <x-home.item-link route="{{ route('register') }}" text="اقدام" />     کنید.</x-home.item>
+                        <x-home.item>جهت ثبت نام از بخش <x-home.item-link route="{{ route('register') }}" text="اقدام" /> کنید.</x-home.item>
                         <x-home.item>مناسب برای مدیران رستوران ها</x-home.item>
+                        
                     </ul>
                     <ul class="flex gap-3 text-sm leading-normal">
-                        <li>
-                            <x-home.button link="{{ route('login') }}" text="ورود کاربران" />
-                        </li>
-                         <li>
-                            <x-home.button text="ورود مدیران" class="!bg-black !hover:bg-stone-500" />
-                        </li>
+                        <li><x-home.button link="{{ route('login') }}" text="ورود کاربران" /></li>
+                         <li><x-home.button text="ورود مدیران" class="!bg-black !hover:bg-stone-500" /></li>
                     </ul>
                 </div>
                 <div class="bg-[#fff2f2] dark:bg-[#1D0002] relative lg:-ms-px -mb-px lg:mb-0 rounded-t-lg lg:rounded-t-none lg:rounded-e-lg! aspect-[335/376] lg:aspect-auto w-full lg:w-[438px] shrink-0 overflow-hidden">
@@ -62,9 +57,5 @@
                 </div>
             </main>
         </div>
-
-        @if (Route::has('login'))
-            <div class="h-14.5 hidden lg:block"></div>
-        @endif
     </body>
 </html>
