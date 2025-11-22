59b6def51e1bd937de3415090f62bcec8ee54409
Mahdi Naderian <naderian.mahdi@gmail.com>
2025-11-22 16:59:57 +0330

    added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('title', config('app.name'))</title>
+        @include('partials.favicon')
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18]">
