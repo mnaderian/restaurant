36dafcfc9b38b37b4a67204477b7fde22bdb0d33
Mahdi Naderian <naderian.mahdi@gmail.com>
2025-11-22 14:28:08 +0330

    updated translation

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,11 +3,11 @@
         <nav class="flex items-center justify-end gap-4">
             @auth
                 <a href="{{ url('/dashboard') }}" class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
-                    {{ __('Dashboard') }}
+                    {{ __('dashboard') }}
                 </a>
             @else
                 <a href="{{ route('login') }}" class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal">
-                    {{ __('Log in') }}
+                    {{ __('log in') }}
                 </a>
 
                 @if (Route::has('register'))
