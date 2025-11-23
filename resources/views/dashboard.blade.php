30a822f9ddb84de67371f2f71e69f7d9e0b3fa7f
Mahdi Naderian <naderian.mahdi@gmail.com>
2025-11-23 12:41:58 +0330

    updated dashboard style

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.app :title="__('Dashboard')">
-    <div class="mt-10 flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+    <div class="mt-8 flex h-full w-full flex-1 flex-col gap-4 rounded-xl pb-5">
         <div class="grid auto-rows-min gap-4 md:grid-cols-3">
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
