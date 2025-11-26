29332e7a25c628a81695364af0ecbe132dd4e773
Mahdi Naderian <naderian.mahdi@gmail.com>
2025-11-26 14:30:01 +0330

    renamed Table model to RestaurantTable

--- database/migrations/2025_11_26_134839_create_restaurant_tables_table.php
+++ database/migrations/2025_11_26_134839_create_restaurant_tables_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tables', function (Blueprint $table) {
+        Schema::create('restaurant_tables', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tables');
+        Schema::dropIfExists('restaurant_tables');
     }
 };
