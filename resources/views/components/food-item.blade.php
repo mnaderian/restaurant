f970ad11b0726c9f5963558c197fce3a084d8cb6
Mahdi Naderian <naderian.mahdi@gmail.com>
2025-12-02 17:00:57 +0330

    wip

--- resources/views/components/food-item.blade.php
+++ resources/views/components/food-item.blade.php
@@ -1,5 +1,7 @@
 <li class="space-x-3">
     <span class="font-bold">{{ $food->name }}:</span>
     <span>{{ $food->price }} تومان</span>
-    <span class="text-xs bg-blue-100 px-3 py-2 rounded-full">{{ $food->description }}</span>
+    @isset($food->description)
+        <span class="text-xs bg-blue-100 px-3 py-2 rounded-full">{{ $food->description }}</span>
+    @endisset
 </li>
